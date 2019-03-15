82cbd097d18294f602c92800c2c4728bae683923
Austin <HunterKillerDrone154@gmail.com>
2019-03-15 16:10:54 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,6 +2,8 @@ module.exports.run = (bot, message, args, Discord) => {
 let em = new Discord.RichEmbed()
 .setTitle("Help Menu!")
 .addField("General Commands:", "help")
+.setColor("GREEN")
+message.channel.send({embed: em})
 }
 
 module.exports.help = {
