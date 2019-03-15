59b37a44dc36995b6dad43162ace287539384eb6
Austin <HunterKillerDrone154@gmail.com>
2019-03-15 16:08:58 -0400

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,9 @@
+module.exports.run = (bot, message, args, Discord) => {
+let em = new Discord.RichEmbed()
+.setTitle("Help Menu!")
+.addField("General Commands:", "help")
+}
+
+module.exports.help = {
+"name": "help"
+}
